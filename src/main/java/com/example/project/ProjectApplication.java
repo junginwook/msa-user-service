fb3c2ee87847833986d33a9ee6d7e6298eb8ecb7
junginwook <inwook94@naver.com>
2021-12-31 09:31:16 +0900

    com.example.project.service.UserServiceImpl getUserByUserId 유저 주문목록 order-servicec와 통신

--- src/main/java/com/example/project/ProjectApplication.java
+++ src/main/java/com/example/project/ProjectApplication.java
@@ -1,11 +1,19 @@
 package com.example.project;
 
+import com.example.project.error.FeignErrorDecoder;
+import feign.Logger;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.client.loadbalancer.LoadBalanced;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 import org.springframework.context.annotation.Bean;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.web.client.RestTemplate;
 
 @SpringBootApplication
+@EnableDiscoveryClient
+@EnableFeignClients //feignClient 라이브러리 사용
 public class ProjectApplication {
 
     public static void main(String[] args) {
@@ -16,4 +24,21 @@ public class ProjectApplication {
     public BCryptPasswordEncoder passwordEncoder() {
         return new BCryptPasswordEncoder();
     }
+
+    @Bean
+    @LoadBalanced
+    public RestTemplate getRestTemplate() {
+        return new RestTemplate();
+    }
+
+    //feign client logger
+    @Bean
+    public Logger.Level feignLoggerLevel() {
+        return Logger.Level.FULL;
+    }
+
+//    @Bean
+//    public FeignErrorDecoder gatFeignErrorDecoder() {
+//        return new FeignErrorDecoder();
+//    }
 }
--- src/main/java/com/example/project/service/UserServiceImpl.java
+++ src/main/java/com/example/project/service/UserServiceImpl.java
@@ -1,18 +1,27 @@
 package com.example.project.service;
 
+import com.example.project.client.OrderServiceClient;
 import com.example.project.dto.UserDto;
+import com.example.project.error.FeignErrorDecoder;
 import com.example.project.jpa.UserEntity;
 import com.example.project.jpa.UserRepository;
 import com.example.project.vo.ResponseOrder;
+import feign.FeignException;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.modelmapper.ModelMapper;
 import org.modelmapper.convention.MatchingStrategies;
 import org.modelmapper.spi.MatchingStrategy;
+import org.springframework.core.ParameterizedTypeReference;
+import org.springframework.core.env.Environment;
+import org.springframework.http.HttpMethod;
+import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Service;
+import org.springframework.web.client.RestTemplate;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -20,12 +29,14 @@ import java.util.UUID;
 
 @Service
 @RequiredArgsConstructor
+@Slf4j
 public class UserServiceImpl implements UserService {
 
     private final UserRepository userRepository;
-
     private final BCryptPasswordEncoder passwordEncoder;
-
+//    private final RestTemplate restTemplate;
+    private final Environment env;
+    private final OrderServiceClient orderServiceClient;
 
     @Override
     public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
@@ -64,8 +75,32 @@ public class UserServiceImpl implements UserService {
         }
         UserDto userDto = new ModelMapper().map(userEntity, UserDto.class);
 
-        List<ResponseOrder> orders = new ArrayList<>();
-        userDto.setOrders(orders);
+//        List<ResponseOrder> orders = new ArrayList<>();
+
+       /* String orderUrl = String.format(env.getProperty("order_service.url"), userId);
+        log.debug("order_service.url = {}", orderUrl);
+        *//*
+        Using as rest template
+         *//*
+      *//*  //요청 파라미터 x
+        ResponseEntity<List<ResponseOrder>> orderListResponse = restTemplate.exchange(orderUrl, HttpMethod.GET, null,
+                new ParameterizedTypeReference<List<ResponseOrder>>(){
+        });*//*
+
+        List<ResponseOrder> orderList = orderListResponse.getBody();*/
+
+        /*
+        feign client 사용
+        feign exception handling
+         */
+
+        List<ResponseOrder> orderList = null;
+
+        /* errorDecoder */
+        //404에러 예외처리
+        orderList = orderServiceClient.getOrders(userId);
+        userDto.setOrders(orderList);
+
 
         return userDto;
     }
